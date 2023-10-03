983e4b10f1d69b8f0edd728611dc5162ecdfa280
joseluisdiaz377 <joseluis.diaz377@alu.uhu.es>
2023-10-03 17:32:02 +0200

    Creando cabeceras de la clase test

--- src/test/EmpleadoTest.java
+++ src/test/EmpleadoTest.java
@@ -27,12 +27,69 @@ class EmpleadoTest {
 	}
 
 	@Test
-	void testCalculoNominaBruta() {
+	void testCalculoNominaBrutaEncargado() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	void testCalculoNominaBrutaVendedor() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaBrutaOtro() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaBrutaVentasMesNegativa() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaBrutaVentasMesMenor1000() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaBrutaVentasMesEntre1000y1500() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaBrutaVentasMesMayor1500() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaBrutaHorasExtrasPositiva() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaBrutaHorasExtrasNegativa() {
+		fail("Not yet implemented");
+	}
+	
+	
 
 	@Test
-	void testCalculoNomina() {
+	void testCalculoNominaNetaNegativa() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaNetaMenor2100() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaNetaEntre2100y2500() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaNetaMayor2500() {
 		fail("Not yet implemented");
 	}
 
